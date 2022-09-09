2cf7239c9b7206b32b424a23c3a0049c49085e4e
Franco Cardenas <gustavo260123@gmail.com>
2022-09-09 08:45:29 -0700

    Adding the completing code to print last digit of number.

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 0:
+    lastdig = number % 10
+else:
+    lastdig = number % -10
+if (lastdig > 5):
+    print("Last digit of", number, "is", lastdig, "and is greater than 5")
+elif (lastdig == 0):
+    print("Last digit of", number, "is", lastdig, "and is 0")
+else:
+    print("Last digit of", number, "is", lastdig, "and is less than 6 and not 0")
