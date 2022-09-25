0cde11853c240e1d7d2b4d1b8d922af308a603d1
gustavofranco26 <gustavo260123@gmail.com>
2022-09-25 02:16:16 -0500

    Adding more fixes intranet

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Created an empty class with named Square"""
 
-  class Square:
+
+class Square:
     """A class Square name"""
+
+
 
     def __init__(self, size):
         self.__size = size
