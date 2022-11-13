e98fe5f13e0caded682e73e9f3fc75fc0c7cf5ad
gustavofranco26 <gustavo260123@gmail.com>
2022-11-13 08:46:38 -0500

    Adding the python file that contains the class definition of a State and an instance Base = declarative_base().

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains one class:
+State - inherited from Base.
+Base is a declarative base instance from sqlalchemy.
+Each instance of State class will be added to a MySQL database.
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class is linked to the states table.
+    Attributes:
+        id: Integer, required, primary key
+        name: String, required
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
