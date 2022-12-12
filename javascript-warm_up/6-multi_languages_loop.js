711efb59d67df30f25a016c28a8ba0a40a066508
gustavofranco26 <gustavo260123@gmail.com>
2022-12-11 21:54:12 -0500

    Adding the script that prints 3 lines: like 1-multi_languages.js.

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const myStringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myStringArray.length; i++) {
+  console.log(myStringArray[i]);
+}
