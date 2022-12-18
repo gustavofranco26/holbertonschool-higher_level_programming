d7469d793acf87750fa5dd03fd3cd9e115add5a2
gustavofranco26 <gustavo260123@gmail.com>
2022-12-18 03:10:35 -0500

    Adding the semistandar usage to fix in file.

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,10 +7,10 @@ request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
-	  fs.writeFile(process.argv[3], body, 'utf8', (err) => {
-		  if (err) {
-			  console.log(err);
-		  }
-	  });
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
