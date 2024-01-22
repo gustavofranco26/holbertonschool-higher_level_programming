1346368cfd1d9b94f7192bc57713e99f20a7b46d
Franco Cardenas <69946309+gustavofranco26@users.noreply.github.com>
2024-01-22 01:13:36 -0500

    Delete SQL_more_queries directory

--- SQL_more_queries/9-cities_by_state_join.sql
+++
@@ -1,5 +0,0 @@
--- Join cities and states tables to display all cities contained in database passed into mysql command
-SELECT cities.id, cities.name, states.name 
-	FROM cities 
-		JOIN states ON (cities.state_id = states.id)
-	ORDER BY cities.id ASC;
